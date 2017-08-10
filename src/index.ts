ef2afd5f42fa109a9ba320d5086296ec786e0e44
Florian Reifschneider <florian@rocketloop.de>
2017-08-10 12:47:19 +0200

    Expose wizard component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './models/wizard-config.model'
 export * from './models/wizard-step-config.model'
 export * from './models/wizard-step.model'
 export * from './ngx-wizard-step/ngx-wizard-step.component';
+export * from './ngx-wizard/ngx-wizard.component';
